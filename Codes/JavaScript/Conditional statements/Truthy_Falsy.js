882ae672f3ff65a87b830f0ba827e417f17be044
Sudeev Divakar <sudeev.divakar@gmail.com>
2023-09-07 22:13:48 +0530

    Finished JavaScript Decision Making

---
+++ Codes/JavaScript/Conditional statements/Truthy_Falsy.js
@@ -0,0 +1,6 @@
+let hello = prompt("enter a number");
+if (hello){
+    console.log("Truthy");
+}else{
+    console.log("Falsey");
+}
---
+++ Codes/JavaScript/Conditional statements/basics.js
@@ -0,0 +1,4 @@
+let rating = Math.random();
+if(rating>=0.5){
+    console.log("HEHEHE_YEA_BUDDYYYYY")
+}
