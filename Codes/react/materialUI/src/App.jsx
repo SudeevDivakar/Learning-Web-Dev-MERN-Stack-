ed00e6e58db46d957e708bba44569d4509d3fd25
Sudeev Divakar <sudeev.divakar@gmail.com>
2023-12-25 14:32:16 +0530

    Finish Material UI

---
+++ Codes/react/materialUI/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import RatingDemo from './RatingDemo';
+import Navbar from './Navbar';
+
+function App() {
+
+
+  return (
+    <>
+      {/* <RatingDemo /> */}
+      <Navbar />
+    </>
+  )
+}
+
+export default App
